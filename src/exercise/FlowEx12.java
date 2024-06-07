49ba12bbeaf3e4c4a81ebfc6d3c7ce217839af98
hyunwoo-9800 <hyunwoo_9800@naver.com>
2024-06-07 19:23:33 +0900

    [add] Java 기초_2024/06/07 @1h

---
+++ src/exercise/FlowEx12.java
@@ -0,0 +1,26 @@
+package exercise;
+
+public class FlowEx12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// i의 값을 출력
+		for (int i = 1; i <= 5; i++) {
+
+			System.out.println(i);
+
+		} // for 끝
+
+		System.out.println();
+		
+		// i의 값을 가로로 출력
+		for (int i = 1; i <= 5; i++) {
+
+			System.out.print(i);
+
+		} // for 끝
+
+	} // main 끝
+
+}
---
+++ src/exercise/FlowEx13.java
@@ -0,0 +1,17 @@
+package exercise;
+
+public class FlowEx13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum = 0; // 합계 저장용 변수
+		
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
+			System.out.printf("1부터 %2d까지의 합 : %2d%n", i, sum);
+		} // for 끝
+		
+	} // main 끝
+
+}

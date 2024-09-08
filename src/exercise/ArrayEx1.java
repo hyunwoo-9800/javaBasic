e781238a9c6bfb2bebfb5c8bb1ca0a47c1ae6d4e
hyunwoo-9800 <hyunwoo_9800@naver.com>
2024-09-08 19:30:31 +0900

    [add] Java 기초_2024/09/08 @1.0h

---
+++ src/exercise/ArrayEx1.java
@@ -0,0 +1,30 @@
+package exercise;
+
+public class ArrayEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] score = new int[5];
+		int k = 1;
+		
+		score[0] = 50;
+		score[1] = 60;
+		score[k + 1] = 70;	// socre[2] = 70;
+		score[3] = 80;
+		score[4] = 90;
+		
+		int tmp = score[k + 2] + score[4]; 	// int tmp = score[3] + [4];
+		
+		for (int i = 0; i < 5; i++) {
+
+			System.out.printf("score[%d] : %d\n", i, score[i]);
+			
+		}
+		
+		System.out.printf("tmp : %d\n", tmp);
+		System.out.printf("score[%d] : %d\n", 7, score[7]); // index의 범위를 벗어난 값
+		
+	} // main 끝
+
+}
---
+++ src/exercise/ArrayEx3.java
@@ -0,0 +1,50 @@
+package exercise;
+
+public class ArrayEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr = new int[5];
+
+		// 배열 arr에 1~5의 값을 저장
+		for (int i = 0; i < arr.length; i++) {
+
+			arr[i] = i + 1;
+
+		} // for 끝
+
+		// 배열에 저장된 값을 출력
+		System.out.println("변경전 - arr.length : " + arr.length);
+
+		for (int i = 0; i < arr.length; i++) {
+
+			System.out.println("arr[" + i + "] : " + arr[i]);
+
+		} // for 끝
+
+		// 배열 arr에 저장된 값을 다른 배열에 복사
+
+		int[] tmp = new int[arr.length * 2];
+
+		for (int i = 0; i < arr.length; i++) {
+
+			tmp[i] = arr[i];
+
+		} // for 끝
+		
+		// tmp에 저장된 값을 arr에 저장
+		arr = tmp;
+
+		// 배열에 저장된 값을 출력
+		System.out.println("변경후 - arr.length : " + arr.length);
+
+		for (int i = 0; i < arr.length; i++) {
+
+			System.out.println("arr[" + i + "] : " + arr[i]);
+
+		} // for 끝
+
+	} // main 끝
+
+}

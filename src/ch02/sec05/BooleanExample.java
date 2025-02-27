dd45bb5d776e8ba8af7f9faea6377c3b57dbdc51
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-02-27 13:45:09 +0900

    [update] Java 기초 2025/02/27 @0.0h

--- src/ch02/sec05/BooleanExample.java
+++ src/ch02/sec05/BooleanExample.java
@@ -16,25 +16,24 @@ public class BooleanExample {
 
 		// if 체크 플래그
 		boolean stop = true;
-		
+
 		if (stop) {
-			
+
 			System.out.println("중지");
-			
+
 		} else {
-			
+
 			System.out.println("시작");
-			
+
 		} // else if 끝
-		
+
 		int x = 10;
 		boolean result1 = (x == 20);
 		boolean result2 = (x != 20);
-		
+
 		System.out.println("result1 = " + result1);
 		System.out.println("result2 = " + result2);
-		
-		
+
 	} // main 끝
 
 } // BooleanExample 끝

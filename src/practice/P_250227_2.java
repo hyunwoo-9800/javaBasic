dcf1e4abec4914acbb19b82da29df2730c087b7b
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-02-27 16:26:41 +0900

    [add] Java 연습 2025/02/27 @0.0h

---
+++ src/practice/P_250227_2.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package practice;
+
+/**
+ * 
+ */
+public class P_250227_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 10 출력
+		int a = 10;
+		System.out.println("a = " + a);
+		
+		// 30 출력
+		int b = 30;
+		System.out.println("b = " + b);
+		
+		// 40 출력
+		int b2 = 30;
+		System.out.println("b2 + 10 = " + (b2 + 10));
+		
+		// 60 출력
+		int c = 30;
+		System.out.println("c + c = " + (c + c));
+		
+		// 80 출력
+		int hello = 50;
+		System.out.println("hello + c = " + (hello + c));
+		
+		// 60 출력
+		int d = 30;
+		d = 60;
+		System.out.println("d = "+ d);
+
+	} // main 끝
+
+} // class 끝

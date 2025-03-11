3b87c872f67526679321b99812031030f47fc741
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-03-11 16:44:25 +0900

    [update] 자바 연습 수정 2025/03/11 @0.0h

--- src/practice/p_250311/exam02/P_250310_04.java
+++ src/practice/p_250311/exam02/P_250310_04.java
@@ -40,23 +40,13 @@ public class P_250310_04 {
 			for (int j = 0; j < array[i].length; j++) {
 
 				sum += array[i][j];
+				
+				// 배열 확인용
+				System.out.println("array[" + i + "]"+ "[" + j + "] = " + array[i][j]);
 
 			}
 
 		}
-		
-		// 배열 확인용 for문
-		for (int i = 0; i < array.length; i++) {
-			
-			for (int j = 0; j < array[i].length; j++) {
-				
-				//System.out.println("array[" + i + "] = " + array[i]);
-				
-				System.out.println("array[" + i + "]"+ "[" + j + "] = " + array[i][j]);
-				
-			}
-			
-		} // for 종료
 		
 		System.out.println();
 		

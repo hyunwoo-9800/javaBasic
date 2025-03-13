a6144b83e7fd085e983802f90771f994df481327
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-03-13 20:32:07 +0900

    [update] 자바 연습 369 해답문 로직 수정

--- src/practice/p_250310/P_250310_02.java
+++ src/practice/p_250310/P_250310_02.java
@@ -25,35 +25,46 @@ public class P_250310_02 {
 			// 값에 3, 6, 9가 포함 되어있다면
 			if (str.contains("3") || str.contains("6") || str.contains("9")) {
 
-				// 값에 33, 36, 39, 63, 69, 99가 포함되어있다면
+				// 값에 33, 36, 39, 63, 66, 69, 99가 포함되어있다면
 				if (str.contains("33") || str.contains("36") || str.contains("39") || str.contains("63")
-						|| str.contains("69") || str.contains("99")) {
-					
+						|| str.contains("66") || str.contains("69") || str.contains("99")) {
+
 					System.out.print("짝짝");
-					
+
+					// 10의 배수라면
+					if (i % 10 == 0) {
+
+						System.out.print("뽀숑");
+					}
+
 				} else {
 
+					// 3, 6 ,9만 포함된 경우
 					System.out.print("짝");
 
+					// 10의 배수라면
+					if (i % 10 == 0) {
+
+						System.out.print("뽀숑");
+					}
+
 				}
+
+			} else if (i % 10 == 0) {
+
+				// 3, 6 ,9가 아니면서 10의 배수일 경우
+				System.out.print("뽀숑");
 
 			} else {
-
-				// 10의 배수라면
-				if (i % 10 == 0) {
-
-					System.out.print("뽀숑");
-
-				} else {
-
-					System.out.print(i);
-
-				}
+				// 3, 6 ,9가 포함되지 않고 10의 배수도 아닌 값
+				System.out.print(i);
 
 			}
-
+			
+			// 개행
 			System.out.println();
-		}
+			
+		} // for 종료
 
 	} // main 끝
 

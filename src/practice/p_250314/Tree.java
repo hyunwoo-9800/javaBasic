4d05c04c4d7db961717eee2101c07e2a76c9b913
hyunwoo-9800 <hyunwoo_9800@naver.com>
2025-03-14 17:27:54 +0900

    [add] 자바 연습 2025/03/14 @0.5h

---
+++ src/practice/p_250314/Tree.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package practice.p_250314;
+
+/**
+ * 
+ */
+public class Tree {
+
+	// 나무 이름
+	String mango = "mango";
+	String papaya = "papaya";
+	String banana = "banana";
+	String lemon = "lemon";
+
+	// 가격
+	int mangoPrice = 200;
+	int papayaPrice = 200;
+	int bananaPrice = 200;
+	int lemonPrice = 200;
+
+	// 걸리는 시간
+	int mangoHour = 4;
+	int papayaHour = 4;
+	int bananaHour = 4;
+	int lemonHour = 4;
+
+	// 산 개수
+	int mangoBuyNum = 7;
+	int papayaBuyNum = 7;
+	int bananaBuyNum = 7;
+	int lemonBuyNum = 7;
+
+	// 살 수 있는 최대 개수
+	int mangoMaxNum = 50;
+	int papayaMaxNum = 50;
+	int bananaMaxNum = 50;
+	int lemonMaxNum = 50;
+
+} // class 끝
